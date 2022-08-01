4572919fc087cdf0f88738356b369efe897c6237
José Lucas Steinmetz da Silveira <65045856+joselucasSteinmetz@users.noreply.github.com>
2022-08-01 13:40:29 -0300

    update
    update

---
+++ CursosCFB/exercicios/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import BBody from './components/BBody'
+
+function App() {
+  return (
+    <>
+      
+      <BBody>
+        <>
+          
+        </>
+      </BBody>
+    </>
+  );
+}
+
+export default App;
---
+++ CursosCFB/exercicios/src/components/BBody.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './BBody.css'
+
+export default function BBody() {
+    return (
+        <section className='Corpo'>
+            Hello there
+        </section>
+    )
+ } 
