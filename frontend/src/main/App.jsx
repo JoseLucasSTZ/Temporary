334d0ad92b45a8095a9eeef0a23478898130dfd5
José Lucas Steinmetz da Silveira <65045856+joselucasSteinmetz@users.noreply.github.com>
2022-08-01 13:41:29 -0300

    update
    update

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import Footer from '../components/template/Footer'
+import Nav from '../components/template/Nav'
+import Logo from '../components/template/Logo'
+import Routes from './Routes'
+
+export default props =>
+    <BrowserRouter>
+        <div className="app">
+            <Logo/>
+            <Nav/>
+            <Routes />
+            <Footer/>
+        </div>
+    </BrowserRouter>
+    
