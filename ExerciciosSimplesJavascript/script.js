544f3a1355383a4c247f797d1edf67c74c0ece38
José Lucas Steinmetz da Silveira <65045856+JoseLucasSTZ@users.noreply.github.com>
2023-05-10 19:40:14 -0300

    update
    Update

---
+++ ExerciciosSimplesJavascript/script.js
@@ -0,0 +1,11 @@
+const botao = document.getElementById("botao");
+const listaItens = document.getElementById("listaItens");
+let contador = 0;
+
+botao.addEventListener('click', () => {
+  contador += 1;
+});
+botao.addEventListener('click', () => {
+
+});
+
