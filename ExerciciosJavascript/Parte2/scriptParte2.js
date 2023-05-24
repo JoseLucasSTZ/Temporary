fe3eb3a639500d46567cc6c810571632ff418113
José Lucas Steinmetz da Silveira <65045856+JoseLucasSTZ@users.noreply.github.com>
2023-05-24 01:17:20 -0300

    Exercícios concluídos
    Exercícios concluídos

--- ExerciciosJavascript/Parte2/scriptParte2.js
+++ ExerciciosJavascript/Parte2/scriptParte2.js
@@ -4,67 +4,75 @@ const selecaoExercicio1 = document.getElementById("selecaoExercicio1");
 const opcaoNaoDestacarExercicio1 = document.getElementById("opcaoNaoDestacarExercicio1");
 const opcaoDestacarExercicio1 = document.getElementById("opcaoDestacarExercicio1");
 const botaoExercicio2 = document.getElementById("botaoExercicio2");
-const textoExercicio2 = document.getElementById("textoExercicio2");
 const paragrafoExercicio2 = document.getElementById("paragrafoExercicio2");
 const arquivoExercicio2 = document.getElementById("arquivoExercicio2");
+const formExercicio3 = document.getElementById("formExercicio3");
+const NomeExercicio3 = document.getElementById("NomeExercicio3");
+const emailExercicio3 = document.getElementById("emailExercicio3");
+const idadeExercicio3 = document.getElementById("idadeExercicio3");
+const botaoExercicio3 = document.getElementById("botaoExercicio3");
+const secao3 = document.getElementById("secao3");
 
 /*Exercício 1*/
 
 botaoExercicio1.addEventListener('click', () => {
-    let opcaoEscolhida = selecaoExercicio1.value;
+  let opcaoEscolhida = selecaoExercicio1.value;
 
-    if (opcaoEscolhida == 'Destacar') {
-        textoExercicio1.classList.add('destacado');
-    } else {
-        textoExercicio1.classList.remove('destacado');
-    };
+  if (opcaoEscolhida == 'Destacar') {
+    textoExercicio1.classList.add('destacado');
+  } else {
+    textoExercicio1.classList.remove('destacado');
+  };
 });
 
 /*Exercício 2*/
-arquivoExercicio2.addEventListener('change', ()=>{
-    
+arquivoExercicio2.addEventListener('change', () => {
+
 })
 
 botaoExercicio2.addEventListener('click', () => {
-   let piada = 
-   paragrafoExercicio2.innerHTML= textoExercicio2.value
+  let ListaPiadas = arquivoExercicio2.files[0];
+  let leitor = new FileReader();
+  let dadosPiadas
+
+  leitor.onload = (evento) => {
+    conteudoArquivo = evento.target.result;
+    dadosPiadas = JSON.parse(conteudoArquivo);
+    let dadosPiadasFormatados = '';
+
+    for (let piada of dadosPiadas.piadas) {
+      dadosPiadasFormatados += ` <p>Pergunta:${piada.pergunta} <br><br> Resposta: ${piada.resposta} <br> -----------------------</p>`
+    }
+    paragrafoExercicio2.innerHTML = dadosPiadasFormatados;
+  }
+  leitor.readAsText(ListaPiadas);
 });
 
 
-/* 
-
-Código em estudo:
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Leitura de arquivo JSON</title>
-  <script>
-    // Função para lidar com a seleção do arquivo
-    function handleFileSelect(event) {
-      const file = event.target.files[0]; // Obtém o arquivo selecionado
-
-      const reader = new FileReader(); // Cria uma instância do FileReader
-
-      reader.onload = function(event) {
-        const jsonContent = event.target.result; // Obtém o conteúdo do arquivo como texto
-        const data = JSON.parse(jsonContent); // Faz o parsing do conteúdo JSON em um objeto JavaScript
-        // Faça algo com os dados do arquivo JSON
-        console.log(data); // Exibe os dados no console
-      };
-
-      reader.readAsText(file); // Lê o conteúdo do arquivo como texto
-    }
-  </script>
-</head>
-<body>
-  <input type="file" accept=".json" onchange="handleFileSelect(event)"> <!-- Input para seleção do arquivo -->
-</body>
-</html>
-
-*/
-
 /*Exercício 3*/
+
+botaoExercicio3.addEventListener('click', (evento) => {
+  evento.preventDefault();
+  let idade = idadeExercicio3.value;
+  let nome = NomeExercicio3.value;
+  let email = emailExercicio3.value;
+
+  if (nome.trim() != '' && idade.trim() != '' && email.trim() != '') {
+    let novoElemento = document.createElement('p');
+    novoElemento.textContent = `Nome: ${nome}, Idade: ${idade}, E-mail: ${email}`;
+    secao3.appendChild(novoElemento);
+  } else {
+    if (nome.trim() === '') {
+      alert('Favor preencher seu nome');
+    }
+    if (idade.trim() === '') {
+      alert('Favor preencher sua idade');
+    }
+    if (email.trim() === '') {
+      alert('Favor preencher seu email');
+    }
+  }
+});
 
 /*Exercício 4*/
 
